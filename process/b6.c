3998cb7761e323d57752ba632dce5afeed79d036
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2026-08-19 15:10:10 +0000

    b6

---
+++ process/b6.c
@@ -0,0 +1,10 @@
+// WAP to show sleep function
+
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(){
+	printf("I am taking a short nap for 10 secs \n");
+	sleep (10);
+	printf("I m back \n");
+}
